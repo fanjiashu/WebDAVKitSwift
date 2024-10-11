c6173062ac3de61e1059e67827a262398ef4b748
fanjiashu <535670110@qq.com>
2024-10-11 16:51:51 +0800

    fix：socks5代理仅限Mac生效

--- Sources/WebDavKit/WebSocket/Socks5ProxyManager.swift
+++ Sources/WebDavKit/WebSocket/Socks5ProxyManager.swift
@@ -9,49 +9,60 @@ import Foundation
 
 public class Socks5ProxyManager {
     public static let shared = Socks5ProxyManager()
-
+    
     private var proxyHost: String?
     private var proxyPort: Int?
     private var isProxyEnabled = false
-
+    
     private init() {}
-
+    
     /// 配置 Socks5 代理
     public  func configureProxy(host: String, port: Int) {
         self.proxyHost = host
         self.proxyPort = port
     }
-
-    /// 开启 Socks5 代理
+    
+    /// 开启 Socks5 代理 ---当前这个仅支持mac
     public func enableProxy() {
+#if os(macOS)
         guard proxyHost != nil, proxyPort != nil else {
             print("Socks5 proxy not configured.")
             return
         }
         isProxyEnabled = true
+#else
+        isProxyEnabled = false
+#endif
     }
-
+    
     /// 关闭 Socks5 代理
     public func disableProxy() {
         isProxyEnabled = false
     }
-
+    
     /// 判断代理是否启用
     public func isProxyActive() -> Bool {
         return isProxyEnabled
     }
-
+    
     /// 获取代理配置的 URLSession 配置
     func getProxySessionConfiguration() -> URLSessionConfiguration? {
+#if os(macOS)
         let configuration = URLSessionConfiguration.default
+        print("代理地址: \(self.proxyHost ?? "未设置")")
+        print("代理端口: \(self.proxyPort ?? 0)")
+        
         // 代理设置
         let proxyDict: [String: Any] = [
-            "SOCKSEnable": 1,
-            "SOCKSProxy": self.proxyHost ?? "代理地址", // SOCKS5代理地址
-            "SOCKSPort": self.proxyPort ?? 1080 // SOCKS5端口
+            kCFNetworkProxiesSOCKSEnable as String: 1,
+            kCFNetworkProxiesSOCKSProxy as String: self.proxyHost ?? "代理地址",
+            kCFNetworkProxiesSOCKSPort as String: self.proxyPort ?? 1080
         ]
-        // 这里配置 SOCKS 代理
         configuration.connectionProxyDictionary = proxyDict
         return configuration
+#else
+        // 其他平台如 iOS 不使用代理
+        return nil
+#endif
     }
 }
